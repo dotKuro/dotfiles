e2f050cad39c2aeb236334045a2c36bc59367faf
Alexander "dotKuro" Kampf <mail@akampf.dev>
2021-01-14 00:25:42 +0100

    Move starup jobs from .xprofile to xmonad

--- .config/xmonad/lib/Autostart.hs
+++ .config/xmonad/lib/Autostart.hs
@@ -1,10 +1,14 @@
 module Autostart (autostart) where
 
-import XMonad(X)
+import XMonad(X. spawn)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
 autostart :: X()
 autostart = do
+  spawn "~/.screenlayout/default.sh"
+  spawn "feh --bg-scale ~/Pictures/gengar-wallpaper-2x.png ~/Pictures/snorlax-wallpaper.jpg"
   spawnOnce "xcompmgr -n -C"
   spawnOnce "udiskie --tray"
   spawnOnce "polybar primary"
+  spawnOnce "xss-lock -- betterlockscreen -l blur"
+
