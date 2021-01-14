6ec92b71da4988be0bd715708ab79ed9184567a3
Alexander "dotKuro" Kampf <mail@akampf.dev>
2021-01-14 22:05:46 +0100

    Adjust config for swapping screens

--- .config/xmonad/lib/Autostart.hs
+++ .config/xmonad/lib/Autostart.hs
@@ -5,7 +5,6 @@ import XMonad.Util.SpawnOnce (spawnOnce)
 
 autostart :: X()
 autostart = do
-  spawn "feh --bg-scale ~/Pictures/gengar-wallpaper-2x.png ~/Pictures/snorlax-wallpaper.jpg"
   spawnOnce "xcompmgr -n -C"
   spawnOnce "udiskie --tray"
   spawnOnce "polybar primary"
