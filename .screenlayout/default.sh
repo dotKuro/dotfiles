e3ddffa907632e9d71888e3587881cb7f2a7d1a1
Alexander "dotKuro" Kampf <mail@akampf.dev>
2021-02-13 16:52:36 +0100

    Adjust for new graphics card

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DVI-D-0 --off
